3200eb79132ed6097296186057b921c95b2cd720
pavangopal <pavangopal39@gmail.com>
2018-06-25 15:08:37 +0530

    Initial Commit

---
+++ iMoneyTests/iMoneyTests.swift
@@ -0,0 +1,36 @@
+//
+//  iMoneyTests.swift
+//  iMoneyTests
+//
+//  Created by Pavan Gopal on 6/25/18.
+//  Copyright © 2018 Pavan Gopal. All rights reserved.
+//
+
+import XCTest
+@testable import iMoney
+
+class iMoneyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
